a6df6d492e33c2407636149927de8f7d5d0122a0
Alberto Arenas <albarenasg@gmail.com>
2024-02-03 15:54:14 -0600

    feat: dockerizing application

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:latest
+
+WORKDIR /app
+COPY . .
+
+RUN go mod download
+RUN go build -o app ./cmd/api/main.go
+
+EXPOSE 3000
+
+CMD ["./app"]
--- internal/repository/database.go
+++ internal/repository/database.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,9 +14,19 @@ var DB *gorm.DB
 func ConnectDB() {
 	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	var db *gorm.DB
+	var err error
+	for i := 0; i < 5; i++ {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		log.Printf("Failed to connect to database: %v. Retrying...", err)
+		time.Sleep(5 * time.Second)
+	}
+
 	if err != nil {
-		log.Fatal("Failed to connect to database: ", err)
+		log.Fatal("Failed to connect to database after retries: ", err)
 	}
 
 	DB = db
